7809e8dad79c6fbfd3ee6178f0da79a72173b6e3
Alyson Mosely <alyson_mosely@yahoo.com>
2022-03-22 12:10:54 -0500

    Lab 08

---
+++ apps/Lab08WorldTrotter/Lab06WorldTrotter/MapViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MapViewController.swift
+//  Lab08WorldTrotter
+//
+//  Created by Alyson Mosely on 3/22/22.
+//
+
+import UIKit
+import MapKit
+class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
+        
+        let segmentedControl
+        = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.systemBackground
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.addTarget(self, action: #selector(mapTypeChanged(_:)), for: .valueChanged)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 8)
+        let margins = view.layoutMarginsGuide
+        let leadingContraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        topConstraint.isActive = true
+        leadingContraint.isActive = true
+        trailingConstraint.isActive = true
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Map View Controller dd Load")
+    }
+    
+    
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+            break
+        }
+    }
+}
